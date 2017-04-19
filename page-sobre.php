6caa17fda116708169a243e4655e09f51096676b
Leandro <lehnihon@hotmail.com>
2017-04-19 15:42:21 -0300

    fix sobre

--- page-sobre.php
+++ page-sobre.php
@@ -1,26 +1,176 @@
 <?php
 get_header(); ?>
-
 <div id="content">
+	<section id="banner">
+		<div class="container">
+			<div class="row">		
+				<div class="col-md-12">
+					<?php putRevSlider( "banner-index" ) ?>
+				</div>
+			</div><br><br>
+		</div>
+	</section>
+	<section class="servicos">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class="small animated bounceIn">Massagens</h1>
+				</div>
+			</div><br>
+			<div class="row">
+			<?php
+			$args = array( 'post_type' => 'produto', 'posts_per_page' => 4, 'orderby' => 'rand', 'tax_query' => array(array('taxonomy' => 'categoria_produto', 'field' => 'slug', 'terms' => 'massagens') )   );
+
+			$myposts = get_posts( $args );
+			$i = 0;
+			foreach ( $myposts as $post ) : setup_postdata( $post );
+			?>
+				<div class="col-md-3 text-left">
+					<?php if ( has_post_thumbnail()) : ?>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+							<?php the_post_thumbnail('home-thumb', array(
+								'class' => "e-claro img-responsive",
+							)); ?>
+						</a>
+					<?php endif; ?>
+					<h4><?php the_title(); ?></h4>
+					<p><?php the_excerpt(); ?></p>
+					<a href="<?php the_permalink(); ?>" class="btn-saiba-mais">saiba mais ></a>
+				</div>
+			<?php 
+			endforeach; 
+			wp_reset_postdata();
+			?>
+			</div>
+		</div>
+	</section>
+
+	<section class="servicosb">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Terapeutas</h2>
+				</div>
+			</div><br>
+			<?php
+			$args = array( 'posts_per_page' => 12, 'offset'=> 0, 'orderby' => 'rand'  );
+
+			$myposts = get_posts( $args );
+			$i = 0;
+			foreach ( $myposts as $post ) : setup_postdata( $post );
+			?>
+				<?php
+				if($i % 4 == 0):
+				?>
+					<div class="row">
+				<?php endif ?>
+				<div class="col-md-3 text-left">
+					<?php if ( has_post_thumbnail()) : ?>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+							<?php the_post_thumbnail('home-thumb', array(
+								'class' => "e-claro img-responsive",
+							)); ?>
+						</a>
+					<?php endif; ?>
+					<h4><?php the_title(); ?></h4>
+					<a href="<?php the_permalink(); ?>" class="btn-saiba-mais">saiba mais ></a>
+				</div>
+	            <?php 
+	            $i++;
+	            if($i % 4 == 0): 
+	            ?>
+					</div><br><br>
+				<?php endif ?>
+			<?php 
+			endforeach; 
+			wp_reset_postdata();
+			?>
+		</div>		
+	</section>
+
+	<section class="servicos">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Estética e Beleza</h2>
+				</div>
+			</div><br>
+			<div class="row">
+			<?php
+			$args = array( 'post_type' => 'produto', 'posts_per_page' => 4, 'orderby' => 'rand', 'tax_query' => array(array('taxonomy' => 'categoria_produto', 'field' => 'slug', 'terms' => 'estetica') )   );
+
+			$myposts = get_posts( $args );
+			$i = 0;
+			foreach ( $myposts as $post ) : setup_postdata( $post );
+			?>
+				<div class="col-md-3 text-left">
+					<?php if ( has_post_thumbnail()) : ?>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+							<?php the_post_thumbnail('home-thumb', array(
+								'class' => "e-claro img-responsive",
+							)); ?>
+						</a>
+					<?php endif; ?>
+					<h4><?php the_title(); ?></h4>
+					<p><?php the_excerpt(); ?></p>
+					<a href="<?php the_permalink(); ?>" class="btn-saiba-mais">saiba mais ></a>
+				</div>
+			<?php 
+			endforeach; 
+			wp_reset_postdata();
+			?>
+			</div>
+		</div>
+	</section>
+
 	<section id="fotos-sobre">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-6 text-left">
-					<h1 class="small azul"><div class="animated bounceIn">Sobre a FRISTAR</div></h1>
-					<p>A Fristar Ar Condicionado, foi fundada em setembro de 2015.<br>
-						Foi através de um trabalho sério, dedicado e com um atendimento diferenciado que conquistou seu espaçõ no mercado, ampliando sua carteira de clientes e os fidelizando.</p>
-					<p>Hoje é reconhecida por sua excelência na prestação de serviços pois investe em produtos de alta qualidade, visando sempre inovar e criar novas formas de atender e satisfazer as necessidades de seus clientes, visando o conforto e o bem-estar.</p>
-					<p>Os seus serviços estão direcionados a instalação de ar condicionado e manutenção em geral, com a qualidade e garantia necessária para atender todos os tipos de consumidores, seja industrial ou residencial aonde a ética, honestidade, caráter, profissionalismo e responsabilidade ambiental, fazem parte do nosso cotidiano equacionando um diferencial no mercado de refrigeração e oferecendo o que há de melhor em climatização.</p>
-					<ul>
-						<li>Manutenção Preventiva</li>
-						<li>Manutenção Corretiva</li>
-						<li>Higienização e Limpeza</li>
-						<li>PMOC</li>
-					</ul>
-					<a href="<?php echo esc_url( home_url( '/' ) )."contato"; ?>" class="btn-confira btn-azul">CONTATO</a>				
+					<h2>Portal dos Terapeutas de São Paulo</h2><br>
+					<p>O Portal dos Terapeutas de São Paulo não é uma clínica ou agência, mas sim, 
+					um portal de divulgação e propaganda dos terapeutas de São Paulo.</p>
+					<p>
+						O Portal dos Terapeutas se trata de um núcleo de profissionais liberais 
+					qualificados que locam nossos espaços e divulgam nesse portal.
+					</p>
+					<p>
+						Os profissionais do Portal dos Terapeutas SP são especializados em massagens 
+					terapêuticas, massagens tântricas, Mix Massagem, Shiatsu, Quick Massagem, 
+					Acumputura e todos os tipos de massagem.
+					</p>
+					<p>
+						Também oferecem cursos de massagens, massagens corporativas, Quiropraxia, 
+					Yoga, banhos de ofurô e serviços de estética e beleza.
+					</p>
+					<p>
+						O Portal dos Terapeutas também oferece o serviço de locação de salas para 
+					massoterapeutas de São Paulo.
+					</p>
+					<p>
+						Você poderá agendar os seus horários de acordo com a sua necessidade.
+					Ao locar as nossas salas, você terá um local requintado e aconchegante para 
+					atender aos seus clientes e dispor de toda a nossa infraestrutura.
+					</p><br>
+					<a href="<?php echo esc_url( home_url( '/' ) )."sobre"; ?>" class="btn-confira margin-responsivo">saiba mais</a>
 				</div>
-				<div class="col-md-6">
-					<img class="e-claro img-responsive" src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/ar-condicionado.jpg"; ?>"/>
+				<div class="col-md-6 text-left">
+					<div class="row">
+						<div class="col-md-6">
+							<img class="e-claro img-responsive" src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/banner-3.jpg"; ?>"/>
+						</div>
+						<div class="col-md-6">
+							<img class="e-claro img-responsive" src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/banner-3.jpg"; ?>"/>
+						</div>
+					</div>
+					<div style="margin-top:25px" class="row">	
+						<div class="col-md-6">
+							<img class="e-claro img-responsive" src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/banner-3.jpg"; ?>"/>
+						</div>
+						<div class="col-md-6">
+							<img class="e-claro img-responsive" src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/banner-3.jpg"; ?>"/>
+						</div>
+					</div>
 				</div>
 			</div>	
 		</div>	
@@ -30,12 +180,13 @@ get_header(); ?>
 		<div class="container">
 			<div class="row text-left detalhes-maps">
 				<div class="col-md-12">
-					<h2 class="branco">Nossa localização</h2>
+					<h2>Nossa localização</h2>
 					<p>
-						Rua Capitão Eugênio de Macedo, 353 - Itaim Paulista - São Paulo - SP
+						Rua Silveira Martins, 160 - Conjunto 02 - Praça da Sé - São Paulo - SP
 					</p>
 					<p>
-						<strong>Telefone.: </strong>11 2561-5068 | 11 98359-1900  <strong>Email.: </strong>carolina@fristar.com.br
+						<strong>Telefone.: </strong>11 3203-1478 | 11 98937-2110
+						<strong>Email.: </strong>contato@portaldosterapeutassp.com.br
 					</p>
 				</div>
 			</div>
@@ -43,7 +194,7 @@ get_header(); ?>
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-md-12 videoWrapper">
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655.0745529549595!2d-46.53579738443502!3d-23.637500970294973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce42b832ebfd53%3A0x6ba710aa9626a2ea!2sR.+Silveira+Martins%2C+160+-+Santa+Teresinha%2C+Santo+Andr%C3%A9+-+SP%2C+09210-520!5e0!3m2!1spt-BR!2sbr!4v1470075254982" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>			
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.4494996124085!2d-46.632920985442624!3d-23.55229438468739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce590747f2413b%3A0x2e7d616150104a8f!2sR.+Silveira+Martins%2C+160+-+S%C3%A9%2C+S%C3%A3o+Paulo+-+SP!5e0!3m2!1spt-BR!2sbr!4v1481463621545" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
 			</div>			
 		</div>
